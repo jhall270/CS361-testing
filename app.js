8b6555f89d3093b02c13a003b54f862dba6f94cb
Jeff Hall <halljeff@oregonstate.edu>
2019-05-27 15:16:54 -0400

    add login outline

--- app.js
+++ app.js
@@ -24,64 +24,64 @@ articles.push(new article("3 Ways to Cut Your Own Hair", "www.nytimes.com/haircu
 articles.push(new article("Are You Harry Potter?", "www.potter.net/harrypottertest-34231"));
 
 
+function login(uid, password){
+  this.uid = uid;
+  this.password = password;
+}
+
+var logins=[];
+logins.push(new login("bobby5","mygoodpassword"));
+logins.push(new login("jj3","myevenbetterpassword"));
+
+
 
 
 //ROUTES
 
+//TODO: home page with links to other parts
 app.get('/',function(req,res){
   res.render('home');
 });
 
+
+//GET route for browse articles
+//Displays list of articles
 app.get('/article-table', function(req, res){
   var context = {};
   context.articles = articles;
   res.render('article-table', context);
-})
-
-/*
-
-app.get('/show-data',function(req,res){
-  var context = {};
-  context.sentData = req.query.myData;
-  res.render('show-data', context);
 });
 
-app.get('/get-loopback',function(req,res){
-  var qParams = "";
-  for (var p in req.query){
-    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
-  }
-  qParams = qParams.substring(0,qParams.lastIndexOf(','));
-  qParams += '.';
+
+
+//This route displays login form
+//form submit posts to verify-login route
+app.get('/login', function(req, res){
   var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback', context);
+
+  res.render('login-form', context);
 });
 
-app.get('/get-loopback-improved',function(req,res){
-  var qParams = [];
-  for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
-  }
+//POST route login forms sends uid/password here to be verified
+app.post('/verify-login', function(req, res){
   var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback-improved', context);
+  uid = req.body.uid;
+  password = req.body.password;
+
+  //TODO:  check request uid and password versus logins array
+  // If is valid, redirect to somewhere
+  // If invalid, do something else, maybe redirect back to login form with error
+  
+
 });
 
-app.post('/post-loopback', function(req,res){
-  var qParams = [];
-  for (var p in req.body){
-    qParams.push({'name':p,'value':req.body[p]})
-  }
-  console.log(qParams);
-  console.log(req.body);
-  var context = {};
-  context.dataList = qParams;
-  res.render('post-loopback', context);
-});
 
-*/
 
+
+
+
+
+//ERROR stuff
 app.use(function(req,res){
   res.status(404);
   res.render('404');
